d61ed9f9fbe4e8623e8dc08e2bcae852644f08e4
antekirta <murashkin.kirill.konst@gmail.com>
2019-08-21 16:10:36 +0200

    Add trees species

--- src/components/Forest/Tree.tsx
+++ src/components/Forest/Tree.tsx
@@ -1,5 +1,38 @@
-class Tree {
-    
+export enum TREES {
+  birch = "birch",
+  beech = "beech",
+  elm = "elm",
+  pearTree = "pearTree",
+  oak = "oak",
+  spruce = "spruce",
+  willow = "willow",
+  chestnut = "chestnut",
+  maple = "maple",
+  linden = "linden",
+  hazel = "hazel",
+  lilac = "lilac",
+  pine = "pine",
+  appleTree = "appleTree",
+  ash = "ash "
 }
 
-export default Tree
+class Tree {
+  private constructor(
+    name: string,
+    toughness: number,
+    flexibility: number,
+    burningHeatQuantity: number,
+    humidity: number
+  ) {}
+
+  static createTree(name: TREES) {
+    let toughness, flexibility, burningHeatQuantity, humidity;
+
+    switch (name) {
+      case TREES.birch:
+        break;
+    }
+  }
+}
+
+export default Tree;
