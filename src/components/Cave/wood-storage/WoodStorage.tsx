7eabcc74aac39da5e0b38b3662c275329b81f595
antekirta <murashkin.kirill.konst@gmail.com>
2019-09-06 15:46:49 +0200

    resources: absolute imports

--- src/components/Cave/wood-storage/WoodStorage.tsx
+++ src/components/Cave/wood-storage/WoodStorage.tsx
@@ -1,5 +1,5 @@
+import { TREES, TREE_PARTS } from "components/Forest/tree/Tree";
 import React from "react";
-import { TREES, TREE_PARTS } from "../../Forest/tree/Tree";
 import TreeBranch from "../../Forest/tree/tree-parts/TreeBranch";
 import { FoldingPanel } from "../../ui-kit/folding-panel/folding-panel";
 
